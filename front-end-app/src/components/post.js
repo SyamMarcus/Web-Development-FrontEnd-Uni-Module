67ddaa6a47e4e28276477d27eec7630ea2d13b15
marcuss <marcuss@uni.coventry.ac.uk>
2021-04-01 23:17:41 +0000

    version 0.2.2
    Feat:
    - Added fetch method to post.js to get listings by ID in the backend

--- front-end-app/src/components/post.js
+++ front-end-app/src/components/post.js
@@ -1,12 +1,70 @@
-import React from 'react'; import { useParams } from 'react-router-dom';
+import React from 'react'; 
+import { withRouter } from 'react-router';
+import { Image, Row, Col, Typography } from 'antd'
 
-function Post(props) { 
-  let { id } = useParams(); 
-  return ( 
-  <> 
-    <h1>Post ID: {id}</h1> 
-    <p>This is where individual article posts can be displayed</p> 
-  </> ); 
+const { Title, Paragraph } = Typography;
+
+class Post extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      post: undefined
+    }
+  }
+
+  componentDidMount() {
+    const id = this.props.match.params.id;
+
+    fetch(`http://localhost:3030/TCS/listings/${id}`)
+    .then(status)
+    .then(json)
+    .then(post => {
+      this.setState({post:post})
+    })
+    .catch(err => {
+      console.log(`Fetch error for post ${id}`)
+    });
+  }
+
+  render() {
+    if (!this.state.post) {
+      return <h3>Loading post...</h3>
+    }
+    const post = this.state.post;
+
+    return (
+      <div>
+        <Row type="flex" justify="space-around" align="middle">
+          <Col span={6} align="center">
+            <Image width={200} alt="Listing Image" src={post.imageURL} />
+          </Col>
+          <Col span={12}>
+            <Title>{post.title}</Title>
+            <Paragraph>{post.breed}</Paragraph>
+            <Paragraph>{post.summary}</Paragraph>
+          </Col>
+        </Row>
+      </div>
+    );
+  }
+
 }
 
-export default Post;
+function status(response) {
+
+  if (response.status >= 200 && response.status < 300) {
+    console.log("status err")
+    return response;
+  } else {
+    return new Promise((resolve, reject) => {
+      return reject(response);
+    });
+  }
+}
+
+function json(response) {
+  return response.json();
+}
+
+export default withRouter(Post);
