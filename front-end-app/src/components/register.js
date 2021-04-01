bb2d215e262123cd491ec44be6b16819973e3db6
marcuss <marcuss@uni.coventry.ac.uk>
2021-04-01 17:54:37 +0000

    version 0.2.0
    Feat:
    - Added validation to the input fields using rules
    - Added console log of the input object on finish

--- front-end-app/src/components/register.js
+++ front-end-app/src/components/register.js
@@ -10,25 +10,62 @@ const tailFormItemLayout = {
   wrapperCol: { xs: { span: 24, offset: 0 }, sm: { span: 16, offset: 6 } },
 };
 
+const emailRules = [
+  {type: 'email', message: 'The input is not valid E-mail!'},
+  {required: true, message: 'Please input your E-mail!' }
+];  
+
+const passwordRules = [  
+  { required: true, message: 'Please input your password!' }
+];  
+  
+const confirmRules = [  
+  { required: true, message: 'Please confirm your password!' },
+  
+  ({ getFieldValue }) => ({
+    validator(rule, value) {
+      if (!value || getFieldValue('password') === value) {
+        return Promise.resolve();
+      }
+      return Promise.reject('The passwords that you entered do not match!');
+    }
+  })
+];
+  
+const usernameRules = [  
+  { required: true, message: 'Please input your username!', whitespace: true }
+];
+
 class RegistrationForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      post: undefined
+    }
+    this.onFinish = this.onFinish.bind(this);
+  }
+
+  onFinish(x) {
+    console.log(x);
+  };
 
   render() {
     return (
-      <Form {...formItemLayout} name="register">
+      <Form {...formItemLayout} name="register" onFinish={this.onFinish} scrollToFirstError>
       
-        <Form.Item name="email" label="E-mail">     
+        <Form.Item name="email" label="E-mail" rules={emailRules}>     
           <Input />      
         </Form.Item>
       
-        <Form.Item name="password" label="Password">     
+        <Form.Item name="password" label="Password" hasFeedback rules={passwordRules}>     
           <Input.Password />     
         </Form.Item>
       
-        <Form.Item name="confirm" label="Confirm Password">      
+        <Form.Item name="confirm" label="Confirm Password" rules={confirmRules}>      
           <Input.Password />      
         </Form.Item>
       
-        <Form.Item name="username" label="Username">      
+        <Form.Item name="userName" label="Username" rules={usernameRules}>      
           <Input />
         </Form.Item>
       
