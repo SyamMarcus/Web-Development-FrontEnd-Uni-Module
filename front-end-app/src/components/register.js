6f6499cf7013eac5de470a383ef17e40203391ac
marcuss <marcuss@uni.coventry.ac.uk>
2021-04-01 16:38:06 +0000

    version 0.1.9
    Refactor:
    - Recreated the register page format by adding a new Form layout with user input and a register button

--- front-end-app/src/components/register.js
+++ front-end-app/src/components/register.js
@@ -1,12 +1,45 @@
 import React from 'react';
+import { Form, Input, Button } from 'antd';
 
-function Register(props) {
-  return (
-    <>
-      <h1>Register</h1>
-      <p>This is where users will be able to register for a new account</p>
-    </>
-  );
-}
+const formItemLayout = {
+  labelCol: { xs: { span: 24 }, sm: { span: 6 } },
+  wrapperCol: { xs: { span: 24 }, sm: { span: 12 } }
+};
 
-export default Register;
+const tailFormItemLayout = {
+  wrapperCol: { xs: { span: 24, offset: 0 }, sm: { span: 16, offset: 6 } },
+};
+
+class RegistrationForm extends React.Component {
+
+  render() {
+    return (
+      <Form {...formItemLayout} name="register">
+      
+        <Form.Item name="email" label="E-mail">     
+          <Input />      
+        </Form.Item>
+      
+        <Form.Item name="password" label="Password">     
+          <Input.Password />     
+        </Form.Item>
+      
+        <Form.Item name="confirm" label="Confirm Password">      
+          <Input.Password />      
+        </Form.Item>
+      
+        <Form.Item name="username" label="Username">      
+          <Input />
+        </Form.Item>
+      
+        <Form.Item {...tailFormItemLayout}>     
+          <Button type="primary" htmlType="submit">     
+            Register     
+          </Button>    
+        </Form.Item >
+      </Form>     
+    );
+  };
+};
+
+export default RegistrationForm;
