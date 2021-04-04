1864ed5641635be1220c46f0feffbb52eef04d4a
marcuss <marcuss@uni.coventry.ac.uk>
2021-04-04 00:22:20 +0000

    version 0.3.0
    Feat:
    - Added validation to the editpost and publish pages
    - Added method of fetching current post data for the editpost form items
    - Added alerts for confimring the changes, deletion and publishing
    - Added placeholder data for the publish listing form

--- front-end-app/src/components/editpost.js
+++ front-end-app/src/components/editpost.js
@@ -15,6 +15,18 @@ const tailFormItemLayout = {
   wrapperCol: { xs: { span: 24, offset: 0 }, sm: { span: 16, offset: 6 } },
 };
 
+const validateMessages = {
+  // eslint-disable-next-line
+  required: '${label} is required!',
+  types: {
+    // eslint-disable-next-line
+    title: '${label} is not a valid title!',
+    // eslint-disable-next-line
+    breed: '${label} is not a valid breed!'
+  }
+};
+
+
 class RegistrationForm extends React.Component {
   constructor(props) {
     super(props);
@@ -24,33 +36,46 @@ class RegistrationForm extends React.Component {
     this.onFinish = this.onFinish.bind(this);
   }
 
+  componentDidMount() {
+    const id = window.location.pathname.split('/')[2];
+    fetch(`http://localhost:3030/TCS/listings/${id}`)
+    .then(status)
+    .then(json)
+    .then(post => {
+      this.setState({post:post})
+    })
+    .catch(err => {
+      console.log(`Fetch error for post ${id}`)
+    });
+  }
+
   onFinish = (values) => {
     const id = window.location.pathname.split('/')[2];
     console.log('Received values of form: ', values);
     const { confirm, ...data } = values;
-    fetch(`http://localhost:3030/TCS/listings/${id}`, {
-      method: "PUT",
-      body: JSON.stringify(data),
-      headers: {
-        "Content-Type": "application/json"
-      }
-    })
-    
-    .then(status)
-    .then(json)
-    .then(data => {
-      console.log(data);
-      alert("Listing Updated")
-    })
-    
-    .catch(errorResponse => {
-      console.error(errorResponse);
-    });  
+    if(window.confirm('Save Listing Changes?')) {
+      fetch(`http://localhost:3030/TCS/listings/${id}`, {
+        method: "PUT",
+        body: JSON.stringify(data),
+        headers: {
+          "Content-Type": "application/json"
+        }
+      })
+      .then(status)
+      .then(json)
+      .then(data => {
+        console.log(data);
+        alert("Listing Updated")
+      })
+      .catch(errorResponse => {
+        console.error(errorResponse);
+      });  
+    } 
   };
   
   deleteListing = () => {
     const id = window.location.pathname.split('/')[2];
-    if(window.confirm('Are you sure?')) {
+    if(window.confirm('Confirm Deletion?')) {
       fetch(`http://localhost:3030/TCS/listings/${id}` , {
         method: "DELETE",
         header:{'Accept':'application/json',
@@ -61,19 +86,32 @@ class RegistrationForm extends React.Component {
   }
 
   render() {
+    if (!this.state.post) {
+      return <h3>Loading post...</h3>
+    }
+    const post = this.state.post;
+
+
     return (
-      <Form {...formItemLayout} name="Finalize" onFinish={this.onFinish} scrollToFirstError>
+      <Form {...formItemLayout} name="Finalize" onFinish={this.onFinish} validateMessages={validateMessages} scrollToFirstError
+        initialValues={{
+          title: post.title,
+          breed: post.breed,
+          summary: post.summary,
+          imageURL: post.imageURL,
+        }}
+      >
       
-        <Form.Item name="title" label="Listing Title" >     
+        <Form.Item name="title" label="Listing Title" rules={[{ required: true }]}>     
           <Input />      
         </Form.Item>
-      
-        <Form.Item name="breed" label="Dog Breed" >     
-          <Input />     
+        
+        <Form.Item name="breed" label="Dog Breed" rules={[{ required: true }]}>     
+          <Input />   
         </Form.Item>
       
         <Form.Item {...formSummaryLayout} name="summary" label="Summary" >      
-          <Input />      
+          <Input.TextArea /> 
         </Form.Item>
       
         <Form.Item name="imageURL" label="Image URL?" >      
@@ -81,8 +119,8 @@ class RegistrationForm extends React.Component {
         </Form.Item>
       
         <Form.Item {...tailFormItemLayout}>     
-          <Button type="primary" htmlType="submit">     
-            Publish    
+          <Button type="primary" htmlType="submit" >     
+            Finalize   
           </Button> 
           <Button type="primary" htmlType="submit" onClick={()=>this.deleteListing()} danger >     
             Delete Listing  
--- front-end-app/src/components/publish.js
+++ front-end-app/src/components/publish.js
@@ -15,6 +15,16 @@ const tailFormItemLayout = {
   wrapperCol: { xs: { span: 24, offset: 0 }, sm: { span: 16, offset: 6 } },
 };
 
+const validateMessages = {
+  // eslint-disable-next-line
+  required: '${label} is required!',
+  types: {
+    // eslint-disable-next-line
+    title: '${label} is not a valid title!',
+    // eslint-disable-next-line
+    breed: '${label} is not a valid breed!'
+  }
+};
 
 class RegistrationForm extends React.Component {
   constructor(props) {
@@ -28,45 +38,47 @@ class RegistrationForm extends React.Component {
   onFinish = (values) => {
     console.log('Received values of form: ', values);
     const { confirm, ...data } = values;
-    fetch('http://localhost:3030/TCS/listings', {
-      method: "POST",
-      body: JSON.stringify(data),
-      headers: {
-        "Content-Type": "application/json"
-      }
-    })
-    
-    .then(status)
-    .then(json)
-    .then(data => {
-      console.log(data);
-      alert("Listing published")
-    })
-    
-    .catch(errorResponse => {
-      console.error(errorResponse);
-      alert(`Error: ${errorResponse}`);
-    });  
-  };
+    if(window.confirm('Confirm Creation?')) {
+      fetch('http://localhost:3030/TCS/listings', {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+          "Content-Type": "application/json"
+        }
+      })
+      
+      .then(status)
+      .then(json)
+      .then(data => {
+        console.log(data);
+        alert("Listing published")
+      })
+      
+      .catch(errorResponse => {
+        console.error(errorResponse);
+        alert(`Error: ${errorResponse}`);
+      });  
+    };
+  }
 
   render() {
     return (
-      <Form {...formItemLayout} name="publish" onFinish={this.onFinish} scrollToFirstError>
+      <Form {...formItemLayout} name="publish" onFinish={this.onFinish} validateMessages={validateMessages} scrollToFirstError>
       
-        <Form.Item name="title" label="Listing Title" >     
-          <Input />      
+        <Form.Item name="title" label="Listing Title" rules={[{ required: true }]}>     
+          <Input placeholder="title"/>      
         </Form.Item>
       
-        <Form.Item name="breed" label="Dog Breed" >     
-          <Input />     
+        <Form.Item name="breed" label="Dog Breed" rules={[{ required: true }]}>     
+          <Input />   
         </Form.Item>
       
         <Form.Item {...formSummaryLayout} name="summary" label="Summary" >      
-          <Input />      
+          <Input.TextArea placeholder="summary"/>   
         </Form.Item>
       
         <Form.Item name="imageURL" label="Image URL?" >      
-          <Input />
+          <Input placeholder="imageURL"/>
         </Form.Item>
       
         <Form.Item {...tailFormItemLayout}>     
