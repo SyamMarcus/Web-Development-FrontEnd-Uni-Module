5a8d0fd37c7ae3e9df82ed530a31e95f10cd14d6
marcuss <marcuss@uni.coventry.ac.uk>
2021-04-11 16:25:49 +0000

    version 0.3.8
    Feat:
    - Added additional form item to the register page for users to add an employee code
    - Added a nested fetch function to the onFinish method of the registration to assign the role of the employee before creating a new user
    
    Refactor:
    - Improved UI of registration page by changing the form item labels

--- front-end-app/src/components/register.js
+++ front-end-app/src/components/register.js
@@ -34,7 +34,7 @@ const confirmRules = [
 ];
   
 const userNameRules = [  
-  { required: true, message: 'Please input your userName!', whitespace: true }
+  { required: true, message: 'Please input your username!', whitespace: true }
 ];
 
 class RegistrationForm extends React.Component {
@@ -48,22 +48,37 @@ class RegistrationForm extends React.Component {
 
   onFinish = (values) => {
     console.log('Received values of form: ', values);
-    const { confirm, ...data } = values;
-    fetch('http://localhost:3030/TCS/register/', {
-      method: "POST",
-      body: JSON.stringify(data),
-      headers: {
-        "Content-Type": "application/json"
-      }
-    })
-    
+    fetch('http://localhost:3030/TCS/register/search?code=' + values.employeeCode)
     .then(status)
     .then(json)
-    .then(data => {
-      console.log(data);
-      alert("User added")
+    .then(code => {
+      if(Object.values(code) === 1) {
+        values.role = 'admin';
+      } else {
+        values.role = 'user';
+      }
+      values.role = 'admin';
+      delete values.employeeCode;
+      const { confirm, ...data } = values;
+      return fetch('http://localhost:3030/TCS/register/', {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+          "Content-Type": "application/json"
+        }
+      })
+      .then(status)
+      .then(json)
+      .then(data => {
+        console.log(data);
+        alert("User added")
+      })
+
+      .catch(errorResponse => {
+        console.error(errorResponse);
+        alert(`Error: ${errorResponse}`);
+      });  
     })
-    
     .catch(errorResponse => {
       console.error(errorResponse);
       alert(`Error: ${errorResponse}`);
@@ -83,6 +98,18 @@ class RegistrationForm extends React.Component {
           <Form.Item name="email" label="E-mail" rules={emailRules}>     
             <Input />      
           </Form.Item>
+
+          <Form.Item name="userName" label="Username" rules={userNameRules}>      
+            <Input />
+          </Form.Item>
+
+          <Form.Item name="firstName" label="First name">      
+            <Input />
+          </Form.Item>
+
+          <Form.Item name="lastName" label="Last name">      
+            <Input />
+          </Form.Item>
         
           <Form.Item name="password" label="Password" hasFeedback rules={passwordRules}>     
             <Input.Password />     
@@ -91,8 +118,8 @@ class RegistrationForm extends React.Component {
           <Form.Item name="confirm" label="Confirm Password" rules={confirmRules}>      
             <Input.Password />      
           </Form.Item>
-        
-          <Form.Item name="userName" label="userName" rules={userNameRules}>      
+
+          <Form.Item name="employeeCode" label="Employee Code">      
             <Input />
           </Form.Item>
         
